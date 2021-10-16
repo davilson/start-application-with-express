2b2f5c177bf199a6e98e26bd2fc202506873c521
Davilson de Castro <davilsondecastro@gmail.com>
2021-10-15 23:03:53 -0300

    refactor: carrega rotas no app express

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { router } from './routes';
+import { createRoutes } from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(router);
+
+createRoutes(app);
 
 export { app };
