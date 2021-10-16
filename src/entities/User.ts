d6dae933f78d5df293f534d333f1a9c96d86ae13
Davilson de Castro <davilsondecastro@gmail.com>
2021-10-15 22:59:40 -0300

    feat: adiciona entidade User

---
+++ src/entities/User.ts
@@ -0,0 +1,18 @@
+import { v4 as uuid } from 'uuid';
+
+export class User {
+    public readonly id: string;
+    public firstname: string;
+    public surname: string;
+    public email: string;
+    public password: string;
+
+    constructor(user: Omit<User, 'id'>, id?: string) {
+        Object.assign(this, user);
+        if (!id) {
+            this.id = uuid();
+        } else {
+            this.id = id;
+        }
+    }
+}
